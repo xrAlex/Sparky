b5815e2c20c371d1f38de0a7353da6326842a529
Alex Grigorjew <grigorjew.shura2011@yandex.ru>
2022-03-13 22:24:42 +0300

    Правка IoC

---
+++ Common/Infrastructure/IoC.cs
@@ -0,0 +1,15 @@
+﻿using SimpleInjector;
+
+namespace Common.Infrastructure
+{
+    public class IoC
+    {
+        private static Container? _instance;
+        public static Container Instance => _instance ??= new Container();
+
+        public static T GetInstance<T>() where T : class
+            => Instance.GetInstance<T>();
+
+        private IoC(){}
+    }
+}
