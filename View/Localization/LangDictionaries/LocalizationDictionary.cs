68175be69fe13c14f37c6c893f97cf5f28d353b3
Alex Grigorjew <grigorjew.shura2011@yandex.ru>
2022-04-11 15:25:02 +0300

    Словари локализации

---
+++ View/Localization/LangDictionaries/LocalizationDictionary.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Localization.LangDictionaries
+{
+    public class LocalizationDictionary : Dictionary<string,string> {}
+}
---
+++ View/Localization/LangDictionaries/RuDict.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Localization.LangDictionaries;
+
+internal sealed class RusDict : LocalizationDictionary
+{
+    public LocalizationDictionary Localization { get; }
+
+    public RusDict()
+    {
+        Localization = new LocalizationDictionary()
+        {
+            {"LocApply", "Применить"},
+            {"LocAutoLaunch", "Запускаться вместе с Windows"},
+            {"LocBrightness", "Яркость"},
+            {"LocCancel", "Отменить"},
+            {"LocDay", "День"},
+            {"LocColorTemperature", "Цветовая температура"},
+            {"LocDontWorkInFullScreen", "Не работать в полноэкранных приложениях"},
+            {"LocExtendedGammaRange", "Расширенный диапозон гаммы"},
+            {"LocNight", "Ночь"},
+            {"LocApplicationsWhiteList", "Список исключений"},
+            {"LocReset", "Сбросить"},
+            {"LocSmoothBrightnessChange", "Плавное изменение гаммы"},
+            {"LocRestart", "Для применения параметров требуется рестарт"},
+            {"LocSunrise", "Восход"},
+            {"LocMonitors", "Мониторы"},
+            {"LocSunset", "Закат"},
+            {"LocToTrayNotification", "Приложение продолжит работу в свернутом состоянии"},
+            {"LocTrayClose", "Закрыть"},
+            {"LocTrayPause", "Пауза"},
+            {"LocTrayUnPause", "Продолжить"}
+        };
+    }
+}
