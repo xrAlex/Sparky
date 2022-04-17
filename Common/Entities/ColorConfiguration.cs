8e05028f554e6255521b97ea54091411fca3553b
Alex Grigorjew <grigorjew.shura2011@yandex.ru>
2022-04-17 19:31:11 +0300

    Для ColorConfiguration добавлен более быстрый метод Equals

--- Common/Entities/ColorConfiguration.cs
+++ Common/Entities/ColorConfiguration.cs
@@ -1,9 +1,17 @@
-﻿namespace Common.Entities;
+﻿using System;
+
+namespace Common.Entities;
 
 public readonly struct ColorConfiguration
 {
     public float Brightness { get; } = 1.0f;
     public float ColorTemperature { get; } = 6000f;
+
+    public bool Equals(ref ColorConfiguration other)
+    {
+        return Math.Abs(Brightness - other.Brightness) < 0.1 
+               && Math.Abs(ColorTemperature - other.ColorTemperature) < 1;
+    }
 
     public ColorConfiguration(float colorTemperature, float brightness)
     {
